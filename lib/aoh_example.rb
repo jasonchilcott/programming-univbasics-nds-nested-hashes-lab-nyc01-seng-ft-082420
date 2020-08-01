d8058847cdd4b597d9a71b99c0eb1e252ddded1b
Jason Chilcott <jasonchilcott@me.com>
2020-08-01 20:15:26 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -28,8 +28,7 @@ end
 def aoh_lookup(aoh, row, key)
 
   assembled_aoh = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
-  aoh[row][:key]
-
+  aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
